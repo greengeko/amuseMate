72d5ab7e0c92830087f3e82057f520b5f4b9da90
Reicela Mackevica <mackevicar@gmail.com>
2024-04-04 18:43:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,46 +1,68 @@
 import requests
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import mindsdb_sdk
 
 con = mindsdb_sdk.connect('http://127.0.0.1:47334')
 databases = con.databases.list()
 
-database = databases[0]  # Database type object
+database = databases[0]
 
 model = con.models.get('lightfm_model')
-
+suggested_items = set()
 app = Flask(__name__)
-
 
 @app.route('/')
 def suggest_activity():
     try:
-        # Define the query for suggestion
-        # query = "SELECT b.* FROM lightfm_suggestions AS b WHERE userId = 4 USING recommender_type = 'user_item'"
+        suggested_items_json = request.args.get('suggested_items')
+        suggested_items_list = suggested_items_json.split(',') if suggested_items_json else []
 
+        # Update suggested_items set
+        if suggested_items_list:
+            suggested_items.clear()
+            suggested_items.update(suggested_items_list)
+
+        # Define the user (hardcoded for now)
         user_id = 4
 
         # Make predictions using the MindsDB model
         predictions = model.predict({'userId': user_id})
 
         recommendations_list = predictions.to_dict(orient='records')
-        item_id = recommendations_list[0]['item_id']
 
-        # Define the endpoint for MindsDB API
-        endpoint = 'http://127.0.0.1:47334/api/sql/query'
+        if recommendations_list:
+            item_id = recommendations_list[0]['item_id']
 
-        query = f"SELECT * FROM files.activities WHERE id = {item_id}"
+            # Check if the item_id has been suggested previously
+            while item_id in suggested_items:
+                # If so, get the next prediction
+                recommendations_list.pop(0)
+                if not recommendations_list:
+                    break
+                item_id = recommendations_list[0]['item_id']
 
-        activity = requests.post(endpoint, json={'query': query})
+            if recommendations_list:
+                # Add the suggested item_id to the set of suggested items to exclude it
+                suggested_items.add(item_id)
 
-        if activity.status_code == 200:
-            result = activity.json()
-            if result:
-                return jsonify({'activity': result})
+                # Define the endpoint for MindsDB API
+                endpoint = 'http://127.0.0.1:47334/api/sql/query'
+                query = f"SELECT * FROM files.activities WHERE id = {item_id}"
+
+                activity = requests.post(endpoint, json={'query': query})
+
+                if activity.status_code == 200:
+                    result = activity.json()
+                    if result:
+                        return jsonify({'activity': result})
+                    else:
+                        return jsonify({'error': 'No activity found'})
+                else:
+                    return jsonify({'error': f'Failed to query MindsDB: {activity.text}'})
             else:
-                return jsonify({'error': 'No activity found'})
+                return jsonify({'error': 'No recommendations available'})
         else:
-            return jsonify({'error': f'Failed to query MindsDB: {activity.text}'})
+            return jsonify({'error': 'No predictions available'})
 
     except Exception as e:
         return jsonify({'error': str(e)})
